134ce52dce0ab2517462327bab622b81246b3f8b
Kilian Seifert Leitch <kilian@theaicore.com>
2025-03-05 19:37:39 +0000

    Adds dummy test

---
+++ src/__tests__/server.test.ts
@@ -0,0 +1,10 @@
+// Basic dummy test file
+describe('Server', () => {
+  it('should pass a dummy test', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should correctly add two numbers', () => {
+    expect(1 + 1).toBe(2);
+  });
+}); 
